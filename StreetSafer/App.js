ccab7791ea3404fb46e87b2889de773cd20b2f60
ali1373107 <alidashti1994a@gmail.com>
2024-02-27 16:50:32 +0000

    icons added  to the side menu items

--- StreetSafer/App.js
+++ StreetSafer/App.js
@@ -1,59 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import WelcomeScreen from './screens/WelcomeScreen';
-import PotholesListScreen from './screens/PotholesListScreen';
-import AddPothole from './screens/AddPothole';
-import ProfileScreen from './screens/ProfileScreen';
-import PotholesOnMap from './screens/PotholesOnMap';
-import {Ionicons} from '@expo/vector-icons'
-import DangerousPothole from './screens/DangerousPotholes';
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import PotholesListScreen from "./screens/PotholesListScreen";
+import AddPothole from "./screens/AddPothole";
+import ProfileScreen from "./screens/ProfileScreen";
+import PotholesOnMap from "./screens/PotholesOnMap";
+import { Ionicons } from "@expo/vector-icons";
+import DangerousPothole from "./screens/DangerousPotholes";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      
-      <Drawer.Navigator screenOptions={{
-        headerStyle: { backgroundColor: '#3c0a6b'},
-        headerTintColor: 'white',
-        drawerActiveTintColor: '#3c0a6b',
-        drawerActiveBackgroundColor:'#f0e1ff',
-        drawerStyle: { backgroundColor: '#cccccc'},
-      }}>
-        <Drawer.Screen name="Profile"  component={ProfileScreen} option={{
-          drawerLabel: 'Profile Screen'
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#3c0a6b" },
+          headerTintColor: "white",
+          drawerActiveTintColor: "#3c0a6b",
+          drawerActiveBackgroundColor: "#f0e1ff",
+          drawerStyle: { backgroundColor: "#cccccc" },
         }}
-        
-        />
-        
-        <Drawer.Screen name="Welcome" component={WelcomeScreen} options={{
-          drawerLabel: 'Welcome Screen',
-         
-        }} />
-        <Drawer.Screen name="User" component={PotholesListScreen}
-        options={{
-          drawerLabel: 'User Screen',
-        
-        }}  />
-        <Drawer.Screen name="AddPothole"  component={AddPothole} option={{
-          drawerLabel: 'Add Pothole Screen'
-        }}
-        
-        />
-        <Drawer.Screen name="DangerousPotholes "  component={DangerousPothole} option={{drawerLabel:"Dangerous Potholes Screen "
-        }}/>
-
-        <Drawer.Screen name="PotholesOnMap"  component={PotholesOnMap} option={{
-            drawerLabel: 'Potholes On Map Screen'
+      >
+        <Drawer.Screen
+          name="PotholesOnMap"
+          component={PotholesOnMap}
+          options={{
+            drawerLabel: "Potholes On Map Screen",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="map" color={color} size={size} />
+            ),
           }}
-        
         />
-        
-        </Drawer.Navigator>
-        
+
+        <Drawer.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            drawerLabel: "Profile Screen",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
+          }}
+        />
+
+        <Drawer.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: "Welcome Screen",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Potholes List"
+          component={PotholesListScreen}
+          options={{
+            drawerLabel: "Potholes List",
+
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="list" color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="AddPothole"
+          component={AddPothole}
+          options={{
+            drawerLabel: "Add Pothole Screen",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="add" color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="DangerousPotholes "
+          component={DangerousPothole}
+          options={{
+            drawerLabel: "Dangerous Potholes Screen ",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="alert-outline" color={color} size={size} />
+            ),
+          }}
+        />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
@@ -61,8 +94,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
