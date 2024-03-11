60275d175ddb30aa9fc30fece0507bc96f98a682
ali1373107 <alidashti1994a@gmail.com>
2024-03-11 19:06:09 +0000

    files, logo added

---
+++ StreetSafer/screens/Login.js
@@ -0,0 +1,10 @@
+import { SafeAreaView, ScrollView, Text, View, StyleSheet } from "react-native";
+import { COLORS, images, FONTS, SIZES } from "../constants";
+
+const Login = () => {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.background }}>
+      <ScrollView></ScrollView>
+    </SafeAreaView>
+  );
+};
